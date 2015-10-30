45621f958419b7cde44c71ac45e3b91868b41303
Di Wu <dw323@cornell.edu>
2015-10-29 23:56:42 -0700

    iterator

--- hc/api/management/commands/sendalerts.py
+++ hc/api/management/commands/sendalerts.py
@@ -16,20 +16,19 @@ class Command(BaseCommand):
     help = 'Sends UP/DOWN email alerts'
 
     def handle(self, *args, **options):
-
-        ticks = 0
         while True:
             # Gone down?
             query = Check.objects
             query = query.filter(alert_after__lt=timezone.now())
             query = query.filter(user__isnull=False)
             query = query.filter(status="up")
-            for check in query:
+            for check in query.iterator():
                 check.status = "down"
 
-                _log("\nSending notification(s) about going down for %s\n" % check.code)
+                _log(
+                    "\nSending notification(s) about going down for %s\n" %
+                    check.code)
                 check.send_alert()
-                ticks = 0
 
                 # Save status after the notification is sent
                 check.save()
@@ -39,15 +38,15 @@ class Command(BaseCommand):
             query = query.filter(alert_after__gt=timezone.now())
             query = query.filter(user__isnull=False)
             query = query.filter(status="down")
-            for check in query:
+            for check in query.iterator():
                 check.status = "up"
 
-                _log("\nSending notification(s) about going up for %s\n" % check.code)
+                _log(
+                    "\nSending notification(s) about going up for %s\n" %
+                    check.code)
                 check.send_alert()
-                ticks = 0
 
                 # Save status after the notification is sent
                 check.save()
 
             time.sleep(30)
-            ticks = (ticks + 1) % 80
