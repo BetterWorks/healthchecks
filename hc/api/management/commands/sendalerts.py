fe3a6c64054bb1b0259f743a721e3362590328ab
Di Wu <dw323@cornell.edu>
2015-10-30 23:36:44 -0700

    send alerts concurrently

--- hc/api/management/commands/sendalerts.py
+++ hc/api/management/commands/sendalerts.py
@@ -2,12 +2,14 @@ import logging
 import sys
 import time
 
+from concurrent.futures import ThreadPoolExecutor
 from django.core.management.base import BaseCommand
+from django.db import connection
 from django.db.models import Q
 from django.utils import timezone
-
 from hc.api.models import Check
 
+executor = ThreadPoolExecutor(max_workers=10)
 logger = logging.getLogger(__name__)
 
 
@@ -16,28 +18,34 @@ def _stdout(message):
     sys.stdout.flush()
 
 
-def handle_one():
-    """ Send an alert for a single check.
-
-    Return True if an appropriate check was selected and processed.
-    Return False if no checks need to be processed.
-
-    """
-
+def handle_many():
+    """ Send alerts for many checks simultaneously. """
     query = Check.objects.filter(user__isnull=False)
 
     now = timezone.now()
     going_down = Q(alert_after__lt=now, status="up")
     going_up = Q(alert_after__gt=now, status="down")
     query = query.filter(going_down | going_up)
-
-    check = query.first()
-    if not check:
+    checks = list(query.iterator())
+    if not checks:
         return False
 
+    for future in [executor.submit(handle_one, check) for check in checks]:
+        future.result()
+
+    return True
+
+
+def handle_one(check):
+    """ Send an alert for a single check.
+
+    Return True if an appropriate check was selected and processed.
+    Return False if no checks need to be processed.
+
+    """
     check.status = check.get_status()
 
-    tmpl = "\nSending alert, status=%s, code=%s\n"
+    tmpl = "Sending alert, status=%s, code=%s\n"
     _stdout(tmpl % (check.status, check.code))
 
     try:
@@ -53,6 +61,7 @@ def handle_one():
         check.status = "paused"
     finally:
         check.save()
+        connection.close()
 
     return True
 
@@ -62,8 +71,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         while True:
-            success = True
-            while success:
-                success = handle_one()
-
+            handle_many()
             time.sleep(30)
