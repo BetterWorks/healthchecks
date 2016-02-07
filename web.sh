304ee24e2697186c984c2fc8592ee1e9791633dd
Di Wu <dw323@cornell.edu>
2016-02-06 23:38:42 -0800

    nginx

--- web.sh
+++ web.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+bin/start-nginx \
 newrelic-admin run-program gunicorn hc.wsgi \
   --max-requests 1000 \
   --max-requests-jitter 50 \
   --preload \
   --timeout 20 \
-  --workers 8
+  --workers 4 \
+  --bind unix:/tmp/nginx.socket \
+  --pid /tmp/app-initialized
