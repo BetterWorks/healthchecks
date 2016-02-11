13437362298b1428e34d0831a834641111fde3df
Di Wu <dw323@cornell.edu>
2016-02-11 10:54:04 -0800

    concurrency 6

--- web.sh
+++ web.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -z "$WEB_CONCURRENCY" ]; then
+  export WEB_CONCURRENCY=6
+  echo "Setting WEB_CONCURRENCY=6"
+fi
+
 bin/start-nginx \
 newrelic-admin run-program gunicorn hc.wsgi \
   --preload \
   --timeout 20 \
-  --workers 4 \
+  --workers $WEB_CONCURRENCY \
   --bind unix:/tmp/nginx.socket \
   --pid /tmp/app-initialized
